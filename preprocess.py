1cff52336b1ea52f258aaac46c32fcbeefcdf23c
adaml <adaml@iri.com>
2023-01-18 16:13:49 -0500

    Annotate without sentence splitting may be needed.

--- preprocess.py
+++ preprocess.py
@@ -38,6 +38,59 @@ class NLP_Preprocessing:
         if not self.initialized:
             self.initialized = True
 
+
+    def annotate_only(self):
+        tab = '\t'
+        newline = '\n'
+        model = None
+        type_str = "pt"
+        if self.model_type == "PYTORCH":
+            model, type_str = nlp.pytorch_model()
+        elif self.model_type == "TENSORFLOW":
+            model, type_str = nlp.tensorflow_model()
+        else :
+            return -1
+        tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name, aggregation_strategy="none")
+        in_file = open(str(self.infile), 'r')
+        out_file = open(str(self.output), "a")
+        line = in_file.readline()
+        text = ""
+        counter = 0
+        while line != "":
+            text = line.strip()
+            if text == "":
+                line = in_file.readline()
+                continue
+            else :
+                line = in_file.readline()
+                counter+=1
+            inputs = tokenizer(text, return_tensors=type_str)
+            tokens = inputs.tokens()
+            if self.model_type == "PYTORCH":
+                outputs = model(**inputs).logits
+                predictions = torch.argmax(outputs, dim=2)
+                for token, prediction in zip(tokens, predictions[0].numpy()):
+                    if (token == "[SEP]" or token == "[CLS]"):
+                        continue
+                    line_in_csv = token + tab +model.config.id2label[prediction] + newline
+                    out_file.write(line_in_csv)
+            elif self.model_type == "TENSORFLOW" :
+                logits = model(**inputs).logits
+                predicted_token_class_ids = tf.math.argmax(logits, axis=-1)
+                predicted_token_class = [model.config.id2label[t] for t in predicted_token_class_ids[0].numpy().tolist()]
+                predictions = predicted_token_class
+                for token, prediction in zip(tokens, predictions):
+                    if (token == "[SEP]" or token == "[CLS]"):
+                        continue
+                    line_in_csv = token + tab + prediction + newline
+                    out_file.write(line_in_csv)
+        else :
+            in_file.close()
+            out_file.close()
+            return 0
+
+    
+
     def preprocess(self):
         tab = '\t'
         newline = '\n'
@@ -54,8 +107,8 @@ class NLP_Preprocessing:
         N = 100
         count = 0
         text = ""
-        out_file = open(self.output, "a")
-        in_file = open(self.infile, 'r')
+        out_file = open(str(self.output), "a")
+        in_file = open(str(self.infile), 'r')
         line = in_file.readline()
         while line != "":
             text = text + line
@@ -116,6 +169,7 @@ class NLP_Preprocessing:
 
             in_file.close()
             out_file.close()
+            return 0
 
 
 
@@ -140,25 +194,30 @@ if __name__ == "__main__":
     ready = True
     array = []
     model_name = args.model_name
-    array[0] = model_name
     model_type = args.model_type
-    array[1] = model_type
     tokenizer_name = args.tokenizer_name
-    array[2] = tokenizer_name
     infile = args.infile
-    array[3] = infile
     output_dir = args.output_dir
-    array[4] = output_dir
     lang = args.language
-    array[5] = lang
+    array.insert(0, model_name)
+    array.insert(1, model_type)
+    array.insert(2, tokenizer_name)
+    array.insert(3, infile)
+    array.insert(4, output_dir)
+    array.insert(5, lang)
     for val in array:
         if val == "" or val == None:
             print("Need to provide all parameters.")
             ready = False
     ready = True
     if ready:
+        success = -1
         nlp = NLP_Preprocessing(model_name, tokenizer_name, model_type, infile, output_dir, lang)
-        success = nlp.preprocess()
+        if str(lang).lower() == "none" :
+            success = nlp.annotate_only()
+        else :
+            success = nlp.preprocess()
+        
         if success == -1:
             print("Failed to process text file.")
         else :
